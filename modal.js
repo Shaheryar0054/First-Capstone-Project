9fdbb20c19314f81f0f1d122b3ccd7fbf9a467b8
Shaheryar Abid <sharyar0310@gmail.com>
2022-10-28 15:56:21 +0500

    Apply the requested changes

--- modal.js
+++ modal.js
@@ -2,7 +2,7 @@ const speakersData = [
   {
     name: 'Yochai Benkler',
     orgnize: 'Professor at Harvard Law School',
-    picture: 'img/speaker_01.png',
+    picture: './img/speaker_01.png',
     about:
         'Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.',
   },
@@ -10,35 +10,35 @@ const speakersData = [
     name: 'Kilnam Chon',
     orgnize:
         'Emeritus Professor, Korea Advanced Institute of Science and Technology (KAIST)',
-    picture: 'img/speaker_02.png',
+    picture: './img/speaker_02.png',
     about:
         "By developing Asia's first Internet protocol network SDN and leading Korea's first private line Internet connection in 1990, it ushered in the era of the Internet in earnest.",
   },
   {
     name: 'Sohyeong Noh',
     orgnize: 'Art Center Nabi Director, CC Korea Director',
-    picture: 'img/speaker_03.png',
+    picture: './img/speaker_03.png',
     about:
         "As the author of <Digital Art Art of Our Time>, he opened 'Art Center Nabi', Korea's first digital art institution in 2000, and is currently serving.",
   },
   {
     name: 'Julia Reda',
     orgnize: 'Head of the Young Pirates of Europe',
-    picture: 'img/speaker_04.png',
+    picture: './img/speaker_04.png',
     about:
         "European integration and online youth participation in politics and democracy are major concerns, and a report has been published that will potentially affect the revision of the EU's copyright law in July.",
   },
   {
     name: 'Lila Tretikov',
     orgnize: 'Secretary General of the Wikimedia Foundation',
-    picture: 'img/speaker_05.png',
+    picture: './img/speaker_05.png',
     about:
         "Layla Tretikov is the general secretary of the Wikimedia Foundation, a non-profit organization that runs Wikipedia. Wikipedia is provided free of charge in 290 languages ​​every month to over 100 million people, nearly half of the world's population.",
   },
   {
     name: 'Ryan Merkley',
     orgnize: 'Creative Commons CEO, Former Mozilla Foundation COO',
-    picture: 'img/speaker_06.png',
+    picture: './img/speaker_06.png',
     about:
         'He led open source projects at the Mozilla Foundation and joined CC as CEO in 2014. He has been active in open movements such as open government and open source.',
   },
